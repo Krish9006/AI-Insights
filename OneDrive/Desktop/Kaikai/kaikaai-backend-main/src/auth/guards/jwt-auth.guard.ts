c43b393f0baa93c7af5c69a37263b1e7aa8dbcc1
Krish9006 <guptashanu341@gmail.com>
2026-08-05 19:49:32 +0530

    Fix build errors for Render deploy

--- OneDrive/Desktop/Kaikai/kaikaai-backend-main/src/auth/guards/jwt-auth.guard.ts
+++ OneDrive/Desktop/Kaikai/kaikaai-backend-main/src/auth/guards/jwt-auth.guard.ts
@@ -8,7 +8,8 @@ export class JwtAuthGuard implements CanActivate {
     
     try {
       // The authentication requires the CLERK_SECRET_KEY to be set in the env
-      const requestState = await clerkClient.authenticateRequest(request, {
+      const requestState = await clerkClient.authenticateRequest({
+        request,
         secretKey: process.env.CLERK_SECRET_KEY,
         publishableKey: process.env.CLERK_PUBLISHABLE_KEY
       });
